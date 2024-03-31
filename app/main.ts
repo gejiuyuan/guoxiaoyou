4c5eeb586a013076d6ea1312477c4b932b6a41b3
gejiuyuan <2507188687@qq.com>
2024-03-31 22:53:35 +0800

    写了个简单的pixi.js案例

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,18 @@
 import '@app/css/index.css';
+import { Application, Graphics } from 'pixi.js';
 
 export {};
+
+const app = new Application({});
+
+await app.init({
+  background: 'cornflowerblue',
+  resizeTo: window,
+});
+
+let obj = new Graphics().rect(500, 200, 400, 300).fill(0xff0000);
+
+// Add it to the stage to render
+app.stage.addChild(obj);
+
+document.getElementById('canvas')!.append(app.canvas);
